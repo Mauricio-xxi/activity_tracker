b3dda60d0f8814fb763b755b992b5d302486f120
Álvaro Camacho <mauricio-xxi@hotmail.com>
2021-03-23 09:10:09 +0100

    minimal refactor, define better the names

--- src/features/ActivitiesTable/ActivitiesTable.jsx
+++ src/features/ActivitiesTable/ActivitiesTable.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState, useCallback } from 'react'
 import { Table, Button } from 'reactstrap'
 import { ACTIVITIES_ENDPOINT } from '../../config'
 import { dataProvider } from '../../dataProvider/dataProvider'
@@ -9,31 +9,39 @@ const ActivitiesTable = () => {
     const [activities, setActivities] = useState()
     const [selectedActivities, setSelectedActivities] = useState({})
     const { addToast } = useToasts()
-    const loadActivitiesFromDB = async () => {
-        const request = {
-            method: 'GET',
-            url: ACTIVITIES_ENDPOINT,
+
+    const loadActivitiesFromDB = useCallback(
+        async () => {
+            const request = {
+                method: 'GET',
+                url: ACTIVITIES_ENDPOINT,
+            }
+            const dbActivities = await dataProvider(request)
+            setActivities(dbActivities)
         }
-        const dbActivities = await dataProvider(request)
-        setActivities(dbActivities)
-    }
+    , [])
+
+    useEffect(() => {
+        loadActivitiesFromDB()
+    }, [loadActivitiesFromDB])
+
     const handlePerformActivities = (e) => {
         const requests = []
         let sendSyncDB = true
-        Object.entries(selectedActivities).forEach((selectedActivity) => {
+        Object.entries(selectedActivities).forEach(([idActivity,isSelected]) => {
             const activityToUpdate = activities.find(
-                (activity) => activity.id === Number(selectedActivity[0])
+                (activity) => activity.id === Number(idActivity)
             )
-            if (selectedActivity[1] && activityToUpdate.covidFriendly) {
+            if (isSelected && activityToUpdate.covidFriendly) {
                 requests.push({
                     method: 'PUT',
-                    url: `${ACTIVITIES_ENDPOINT}/${selectedActivity[0]}`,
+                    url: `${ACTIVITIES_ENDPOINT}/${idActivity}`,
                     data: {
                         ...activityToUpdate,
                         timesPerformed: activityToUpdate.timesPerformed + 1,
                     },
                 })
-            } else if (selectedActivity[1] && !activityToUpdate.covidFriendly) {
+            } else if (isSelected && !activityToUpdate.covidFriendly) {
                 addToast(
                     `Sorry but you can't perform ${activityToUpdate.name} during the COVID`,
                     {
@@ -64,9 +72,6 @@ const ActivitiesTable = () => {
             [id]: selected,
         })
     }
-    useEffect(() => {
-        loadActivitiesFromDB()
-    }, [])
 
     return (
         <div className={'container'}>
